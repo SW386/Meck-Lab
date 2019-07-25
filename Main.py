9ea1121e0024af0121b64ce8438060d58ca14794
SW386 <49171377+SW386@users.noreply.github.com>
2019-07-25 18:33:20 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,6 +10,15 @@ import Plot
 
 
 def run_analysis(cond, fit = True, normalize = True, multi = False, single_trial = False, save = False):
+    """
+    cond is the session type, PI or FI
+    fit is whether the data should be fit to a guassian curve
+    normalize is whether the data should be divided by the max value
+    multi is whether 1 trial should be graphed or many
+    single_trial indicates if the start and stop instances of high press volume should be identified
+    save indicates whether the plot of the data should be saved or not
+    """
+    
     cropped = Process_1.Process_Raw(root = 'C:\\Users\\Shufan Wen\\Desktop\\Test', loc = 'Unprocessed', dest = '')
     steps = Plot_1.plotMulti(cropped, cond, multi, single_trial, normalize, fit, save)
     return steps
