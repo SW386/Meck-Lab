e9d7627bf13d514d7ea2b88b053c3ab5321c866a
Shufan Wen <shufan.wen@duke.edu>
2019-09-18 15:43:51 -0400

    Changed Dictionary

--- Experiment.py
+++ Experiment.py
@@ -16,32 +16,35 @@ class Process_Raw:
     def __init__(self, root, loc, dest):
         self.root = root
         self.path = dest
-        self.subjects = defaultdict(list)
+        self.experiments = defaultdict(list)
         self.identifier = defaultdict(list)
 
         for fpath in glob.glob(os.path.join(self.root, loc, '*')):
             if os.path.isfile(fpath) and fpath[-3:] != 'csv':
                 with open(fpath) as f:
                     fdata = f.read()
-                    subject = re.findall(r'(?<=Experiment: ).*', fdata)
+                    experiment = re.findall(r'(?<=Experiment: ).*', fdata)
+                    subject = re.findall(r'(?<=Subject: ).*', fdata)
                     date  = re.findall(r'(?<=Start Date: ).*', fdata)
                     cond = re.findall(r'PI|FI', fdata)
-                    meta = '_'.join([subject[0], cond[0], date[0].replace('/', '_')])
+                    meta = '_'.join([experiment[0], cond[0], date[0].replace('/', '_')])
                     data = re.findall(r'([0-9]{1,})[\s.]', fdata.split('U:', 1)[1])
-                    data = np.array(data).reshape((int((len(data)) / 2), 2))     
-                self.identifier[subject[0]].append(date[0].replace('/','_'))
-                self.subjects[subject[0]].append(data)
+                    data = np.array(data).reshape((int((len(data)) / 2), 2))
+                    final_id = (subject, date[0].replace('/','_'))
+                    final_data = (subject, data)
+                self.identifier[experiment[0]].append(final_id)
+                self.experiments[experiment[0]].append(final_data)
                 
-                subject_path = os.path.join(self.root, 'Subjects', subject[0])
+                experiment_path = os.path.join(self.root, 'Experiments', experiment[0])
                 session_types = ['FI', 'PI']
                 for i in range(2):
-                    if not os.path.exists(os.path.join(subject_path, 'Processed', session_types[i])):
-                           os.makedirs(os.path.join(subject_path, 'Processed', session_types[i]))
-                    if not os.path.exists(os.path.join(subject_path, 'Raw', session_types[i])):
-                           os.makedirs(os.path.join(subject_path, 'Raw', session_types[i]))
+                    if not os.path.exists(os.path.join(experiment_path, 'Processed', session_types[i])):
+                           os.makedirs(os.path.join(experiment_path, 'Processed', session_types[i]))
+                    if not os.path.exists(os.path.join(experiment_path, 'Raw', session_types[i])):
+                           os.makedirs(os.path.join(experiment_path, 'Raw', session_types[i]))
                            
-                processed_path = os.path.join(subject_path, 'Processed', cond[0], meta + '.txt')
-                raw_path = os.path.join(subject_path, 'Raw', cond[0])
+                processed_path = os.path.join(experiment_path, 'Processed', cond[0], meta + '.txt')
+                raw_path = os.path.join(experiment_path, 'Raw', cond[0])
                 np.savetxt(processed_path, data, fmt = '%s')
                 shutil.copy(fpath, raw_path)       
             
@@ -49,9 +52,9 @@ class Process_Raw:
 
         formatted = defaultdict(list)
 
-        for sub, data in self.subjects.items():
+        for exp, data in self.experiments.items():
             for session in data:
-                all_data = session.astype(int)
+                all_data = session[1].astype(int)
                 session_data = pd.DataFrame()
                 curr_trial = 0               
                 for event, press in all_data[1:, :]:
@@ -68,6 +71,6 @@ class Process_Raw:
                         new = pd.DataFrame([left_press, right_press], index = index) #Creates new DataFrame with MultiIndex
                         new = new.T #Transpose DataFrame
                         session_data = pd.concat([session_data, new], axis = 1) #Combines 2 DataFrames
-                        
-                formatted[sub].append(session_data)     
+                formatted[exp].append((session_data, session[0]))     
+                
         return formatted
